6f0f59f2051093c057dcc30df5590930eff27e82
Phanis <Phanis@users.noreply.github.com>
2018-07-11 09:26:41 +0300

    Update HJM_HW.py

--- RiskAnalytics/HJM_HW.py
+++ RiskAnalytics/HJM_HW.py
@@ -3,21 +3,15 @@ from numpy.random import normal
 
 
 class HW:
-#   sigma: constant volatility parameter
-#   kappa: volatility mean reversion
-#   dt: time step
-#   Libor_rates: for maturities less than one year
-#   Swap_rates: for maturities greater than one year
-#   maturity: in years
+    
     def __init__(self,sigma, kappa, dt, Libor_rates, Swap_rates, maturity):
-        self.sigma = sigma
-        self.kappa = kappa
-        self.dt = dt
+        self.sigma = sigma #   sigma: constant volatility parameter
+        self.kappa = kappa#   kappa: volatility mean reversion
+        self.dt = dt#   dt: time step
         self.forward_rate = Swap_rates
-        self.Libor_rates = Libor_rates
-        self.Swap_rates = Swap_rates
-        self.maturity = maturity
-
+        self.Libor_rates = Libor_rates#   Libor_rates: for maturities less than one year
+        self.Swap_rates = Swap_rates#   Swap_rates: for maturities greater than one year
+        self.maturity = maturity#   maturity: in years
 
     def buildForwardCurve(self,Libor_rates, Swap_rates):
         self.forward_rate = Swap_rates
@@ -26,7 +20,7 @@ class HW:
     def simulateShortRate(self):
         for t in range(1, self.maturity/self.dt):
             r(t + 1)=r(t)+self.forward_rate(t+1)-self.forward_rate(t)
-            r(t + 1) = r(t + 1) + (self.sigma ^ 2 * (1 - exp(-2 * self.kappa * (t + 1))) / (2 * self.kappa)) * self.dt
-            r(t + 1) = r(t + 1) + self.kappa * self.dt * (self.forward_rate(t)- r(t))
-            r(t + 1) = r(t + 1) + self.sigma * sqrt(self.dt) * normal(0,1,1)
+            r(t + 1) += (self.sigma ^ 2 * (1 - exp(-2 * self.kappa * (t + 1))) / (2 * self.kappa)) * self.dt
+            r(t + 1) += self.kappa * self.dt * (self.forward_rate(t)- r(t))
+            r(t + 1) += self.sigma * sqrt(self.dt) * normal(0,1,1)
         return r
